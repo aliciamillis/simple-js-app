69c291795ec89660648c6c46e197ba7c5f6d53fd
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2020-08-24 11:49:33 -0600

    created pokemon list

--- js copy/scripts.js
+++ js copy/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello World!');
-
-var favFood = 'Pizza';
-document.write(favFood);
+var pokemonList = [
+  {name: 'Evevee', height: 0.3, color: 'brown and yellow', abilities: ['run-away', 'adaptable']}
+  {name: 'Pikachu', height: 0.4, color: 'yellow', abilities: ['static', 'lightningrod']}
+  {name: 'Wigglytuff', height: 1, color: 'pink', abilities: ['cute-charm', 'frisk']}
+]
