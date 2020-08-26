0bccc1fb6cda29434a7c73651ec3f96456b2e4af
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2020-08-26 16:24:02 -0600

    created for loop to show small pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
-var pokemonList = [
-  {name: 'Evevee', height: 0.3, color: 'brown and yellow', abilities: ['run-away', 'adaptable']}
-  {name: 'Pikachu', height: 0.4, color: 'yellow', abilities: ['static', 'lightningrod']}
+let pokemonList = [
+  {name: 'Evevee', height: 0.3, color: 'brown and yellow', abilities: ['run-away', 'adaptable']},
+  {name: 'Pikachu', height: 0.4, color: 'yellow', abilities: ['static', 'lightningrod']},
   {name: 'Wigglytuff', height: 1, color: 'pink', abilities: ['cute-charm', 'frisk']}
 ]
-
-document.write(pokemonList);
+for (let i=0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 0.5){
+    console.log(pokemonList[i].name + " is so small!");
+  }
+}
