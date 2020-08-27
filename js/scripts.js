0b104c37411c7f8eb67b3edab3161e64f7a38740
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2020-08-27 13:38:24 -0600

    made list appear on site

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
+
+//this is my pokemon list
 let pokemonList = [
-  {name: 'Evevee', height: 0.3, color: 'brown and yellow', abilities: ['run-away', 'adaptable']},
-  {name: 'Pikachu', height: 0.4, color: 'yellow', abilities: ['static', 'lightningrod']},
-  {name: 'Wigglytuff', height: 1, color: 'pink', abilities: ['cute-charm', 'frisk']}
+  {name: 'Evevee ', height: 0.3 , color: 'brown and yellow ', abilities: ['run-away ', 'adaptable ']},
+  {name: 'Pikachu ', height: 0.4 , color: 'yellow ', abilities: ['static ', 'lightningrod ']},
+  {name: 'Wigglytuff ', height: 1 , color: 'pink ', abilities: ['cute-charm ', 'frisk ']}
 ]
-//this loop shows which pokemon are small 
+document.write('<h2>===Pokemon List===</h2>');
+document.write("<p>" + "name: " + pokemonList.name + "height: " + pokemonList.height + '<br>' + "color: " + pokemonList.color + "abilities: " + pokemonList.abilities + "</p>");
+
+//this loop shows which pokemon are small
 for (let i=0; i < pokemonList.length; i++) {
   if (pokemonList[i].height < 0.5){
+    document.write("<p>" + "name: " + pokemonList[i].name + "height: " + pokemonList[i].height + '<br>' + "color: " + pokemonList[i].color + "abilities: " + pokemonList[i].abilities + "</p>");
     console.log(pokemonList[i].name + " is so small!");
   }
 }
