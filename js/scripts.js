3ac0a536f39ba85a4cd68a7760404214e8aecad9
aliciamillis <69804543+aliciamillis@users.noreply.github.com>
2020-08-28 11:43:18 -0600

    added style, buttons and DOM interaction

--- js/scripts.js
+++ js/scripts.js
@@ -24,16 +24,44 @@ var pokemonRepository = (function () {
     function getAll() {
       return pokemonList;
     }
+
     function add(pokemon) {
+      if (
+        typeof pokemon === "object" &&
+        "name" in pokemon &&
+        "height" in pokemon &&
+        "color" in pokemon &&
+        "abilities" in pokemon
+      ) {
       pokemonList.push(pokemon);
+      } else {
+        console.log("pokemon is not correct");
+      }
     }
+
+    function addListItem(pokemon){
+      let pokemonList = document.querySelector(".pokemon-list");
+      let listPokemon = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = pokemon.name;
+      button.classList.add("button");
+      listPokemon.appendChild(button);
+      pokemonList.appendChild(listPokemon);
+      document.createElement("button").addEventListener("click", showDetails);
+    }
+
+    function showDetails(pokemon){
+      console.log(pokemon);
+    }
+
     return {
       getAll: getAll,
-      add: add
-    };
+      add: add,
+      addListItem: addListItem
+    }
+    ;
   })();
 
-getAll.forEach(function(pokemon){
-  document.write("<p>" + "name: " + pokemon.name + "height: " + pokemon.height +
-  '<br>' + "color: " + pokemon.color + "abilities: " + pokemon.abilities + "</p>");
+pokemonRepository.getAll().forEach(function(pokemon){
+  pokemonRepository.addListItem(pokemon);
 });
